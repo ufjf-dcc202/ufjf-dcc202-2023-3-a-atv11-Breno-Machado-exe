2aa72aad0d968164f8dd39af72e7e253eb458927
Breno Machado <breno.machado@estudante.ufjf.br>
2023-12-05 00:37:44 -0300

    adiciona alterações no js

--- lista.js
+++ lista.js
@@ -1,18 +1,15 @@
 
 const lista = ["Um", "Dois", "Três"];
 
-export function getLista()
-{
-    return lista;
+export function getLista(){
+    return structuredClone(lista);
 }
 
-export function limparLista()
-{
+export function limpaLista(){
     lista.splice(0);
 }
 
-export function adicionarNaLista(novoItem)
-{
-    lista.push(novoItem)
+export function adicionaNaLista(novoItem){
+    lista.push(novoItem);
 }
-//export {getLista};
+
--- main.js
+++ main.js
@@ -1,40 +1,35 @@
-// main.js
-import {adicionarNaLista, getLista, limparLista} from "./lista.js";
+import { getLista, limpaLista, adicionaNaLista} from "./lista.js"
 
-//passo 1 - Ache os elementos importantes (pelo inspecionar do google)
+const olItens = document.querySelector('#itens');
 const pEntrada = document.querySelector('#entrada');
-const olSaida = document.querySelector('#itens');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-btnLimpar.addEventListener('click',limparElementosDaLista);
-btnAdicionar.addEventListener('click',criaElementoNaLista);
+atualizarLista();
 
-atualizarItensDeLista();
-
-function criaElementoNaLista()
-{
-    const texto = pEntrada.textContent;
-    adicionarNaLista(texto);
-    atualizarItensDeLista();
-    pEntrada.textContent="";
-    pEntrada.focus();
-}
-
-function limparElementosDaLista(){
-    limparLista();
-    atualizarItensDeLista();
-}
-
-function atualizarItensDeLista ()
-{
-    olSaida.innerHTML= "";  
-    const lista = getLista();
-    for (let i = 0; i < lista.length; i++) {
-        const item = lista[i];
-        const li =  document.createElement('li');
-        li.textContent = item;
-        olSaida.appendChild(li);
+function atualizarLista(){
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for(let i = 0; i < lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
     }
 }
 
+btnLimpar.addEventListener('click', limparItensDeLista);
+
+function limparItensDeLista(){
+    limpaLista();
+    atualizarLista();
+}
+
+btnAdicionar.addEventListener('click', criaElementoNaLista)
+
+function criaElementoNaLista(){
+    const texto = pEntrada.textContent
+    adicionaNaLista(texto)
+    atualizarLista()
+    pEntrada.textContent=""
+    pEntrada.focus()
+}
